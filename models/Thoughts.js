54015a01a013634a51d2a1d9ea2524242c1f60e1
Beatriz Sandoval <beasaddress@gmail.com>
2023-08-17 17:33:09 -0700

    made the models based on challenge instructions, imported express and connection to mongoose into the server side. Going to start working on the routes

---
+++ models/Thoughts.js
@@ -0,0 +1,38 @@
+const {Schema, model } = require('mongoose');
+
+const thoughtSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            minlength: 1,
+            maxlength: 280,
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: (timestamp) => {
+                return timestamp.toISOString();
+            },
+        },
+        username: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'user',
+            required: true,
+        },
+        reactions: [{ type: Schema.Types.ObjectId, ref: 'reactions' }],
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true,
+          },
+          id: false,  
+    }
+);
+thoughtSchema.virtual('reactionCount').get(function() {
+    return this.reactions.length;
+});
+
+const Thoughts = model('thoughts', thoughtSchema);
+module.exports = Thoughts;
---
+++ models/User.js
@@ -0,0 +1,33 @@
+const {Schema, model } = require('mongoose');
+
+const userSchema = new Schema({
+    username: { 
+        type: String,
+        required: true,
+        unique: true,
+        trimmed: true,
+    },
+    email: {
+        type: String,
+        required: [true, 'Please enter a valid email address'],
+        unique: true,
+        match: /.+\@.+\..+/,
+    },
+    thoughts: [{ type: Schema.Types.ObjectId, ref: 'thoughts'}],
+    friends: [{ type: Schema.Types.ObjectId, ref: 'user'}],
+
+    },
+    {
+        toJSON: {
+            virtuals: true,
+        },
+        id: false,
+    }
+);
+userSchema.virtual('friendCount').get(function () {
+    return this.friends.length;
+});
+
+const User = model('user', userSchema);
+
+module.exports = User;
